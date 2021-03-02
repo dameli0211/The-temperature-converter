1e5c56e6b6ba959672dd6ccbfd5cf55050fa754c
dameli0211 <dameli0211@gmail.com>
2021-03-02 18:54:48 -0500

    added unicode

--- temperature.js
+++ temperature.js
@@ -1,8 +1,8 @@
 var farhenheit = 19*(9/5)+32;
-console.log("It is " + farhenheit + " F outside");
+console.log("It is " + farhenheit + "\u00B0F outside.");
 
 var celsius = (66.2-32)*5/9;
-console.log("It is " + celsius + " C outside");
+console.log("It is " + celsius + "\u00B0C outside.");
  
 
 console.log(Math.floor(Math.random(farhenheit)* 101));
