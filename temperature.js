90722f23cf6e7e63231349cd9fdb824dcbf7b7db
dameli0211 <dameli0211@gmail.com>
2021-03-02 18:33:16 -0500

    switched from celsiuse to farhenheit and viceversa and concatenate them

--- temperature.js
+++ temperature.js
@@ -1,8 +1,8 @@
-var celsius = 19*(9/5)+32;
-console.log(celsius);
+var farhenheit = 19*(9/5)+32;
+console.log("It is " + farhenheit + " F outside");
 
-var fahrenheit = (66.2-32)*5/9;
-console.log(fahrenheit);
+var celsius = (66.2-32)*5/9;
+console.log("It is " + celsius + " C outside");
  
 
-console.log(Math.floor(Math.random(fahrenheit)* 101));
+console.log(Math.floor(Math.random(farhenheit)* 101));
